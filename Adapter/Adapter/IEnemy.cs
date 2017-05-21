ca44d729de96f44e24887dcf45dab82495e5532a
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-22 01:22:55 +0300

    Added IEnemy Interface

---
+++ Adapter/Adapter/IEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public interface IEnemy
+    {
+        void useGun();
+        void run();
+        void enemyName();
+
+
+    }
+}
