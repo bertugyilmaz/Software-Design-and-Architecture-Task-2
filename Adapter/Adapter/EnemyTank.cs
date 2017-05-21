f6fbec1c137da6d91d02c2ddf94328b44bdd62a4
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-22 01:33:04 +0300

    Added EnemyTank Class

---
+++ Adapter/Adapter/EnemyTank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class EnemyTank : IEnemy
+    {
+        public void enemyName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void useGun()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
