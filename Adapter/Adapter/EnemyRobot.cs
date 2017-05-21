b8fc0da3455ed3ac3a18ec92296644c24ad237ad
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-22 01:25:38 +0300

    Added EnemyRobot class

---
+++ Adapter/Adapter/EnemyRobot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class EnemyRobot : IEnemy
+    {
+        public void enemyName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void useGun()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Adapter/Adapter/IEnemy.cs
+++ Adapter/Adapter/IEnemy.cs
@@ -12,6 +12,5 @@ namespace Adapter
         void run();
         void enemyName();
 
-
     }
 }
